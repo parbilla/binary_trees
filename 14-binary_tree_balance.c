c576e749277390270385a3fd5961bc3773a2e820
parbilla <parbilla@gmail.com>
2021-03-11 17:30:09 +0000

    task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,6 +7,25 @@
  * Return: balance factor
  */
 
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t hl = 0;
+	size_t hr = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->right == NULL && tree->left == NULL)
+		return (0);
+	hl = binary_tree_height(tree->left);
+	hl++;
+	hr = binary_tree_height(tree->right);
+	hr++;
+	if (hl > hr)
+		return (hl);
+	else
+		return (hr);
+}
+
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	int hl = 0;
