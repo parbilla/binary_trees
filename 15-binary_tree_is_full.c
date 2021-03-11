0bdf63a5a2c19a694e9946e7b3e7d6ba3bbd85f2
parbilla <parbilla@gmail.com>
2021-03-11 18:40:10 +0000

    task 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -13,13 +13,12 @@ int binary_tree_is_full(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	if ((tree->left == NULL && tree->right != NULL) ||
-	    (tree->left != NULL && tree->right == NULL))
-		return (0);
+	if (tree->left != NULL && tree->right != NULL) 
+	{
+		full = binary_tree_is_full(tree->left);
+		full = binary_tree_is_full(tree->right);
+	}
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-
-	full = binary_tree_is_full(tree->left);
-	full = binary_tree_is_full(tree->right);
 	return (full);
 }
